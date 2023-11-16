3afbcfd885121fab27bc7cb4bb5f2b28e61c06b0
213210570 <150804388+213210570@users.noreply.github.com>
2023-11-16 18:15:07 +0800

    Add files via upload

--- wrinkle.py
+++ wrinkle.py
@@ -8,8 +8,8 @@ from skimage import measure, morphology
 import dlib
 
 
-def master_control(image,theta):
-    b, g, r = cv2.split(image)
+def master_control(g,theta):
+    #b, g, r = cv2.split(image)
 
     sk_frangi_img = frangi(g, sigmas=[0,1,0.01], beta=1.5, gamma=0.01)
     sk_frangi_img = morphology.closing(sk_frangi_img, morphology.disk(1))
@@ -47,14 +47,12 @@ def master_control(image,theta):
     return skels
 
 
-def face_wrinkle(path,theta=0):
-    result = cv2.imread(path)
+def face_wrinkle(result,theta=0):
+    #result = cv2.imread(path)
     img = master_control(result,theta)
     count=0
     result[img > 0.1] = 255
     img=img.astype(float)
-    cv2.imshow('ss',img)
-    cv2.waitKey()
     for row in img:
         for i in row:
             if not i==0:
@@ -62,12 +60,62 @@ def face_wrinkle(path,theta=0):
 
     return count/(img.shape[0]*img.shape[1])
 
-
-
+def get_Wrinkle_vector(face_image_path):
+    img=cv2.imread(face_image_path)
+    b, gray, r = cv2.split(img)
+    dets = detector(img, 1)
+    landmarks=[]
+    result=[]
+    print("Number of faces detected: {}".format(len(dets)))
+    for k, d in enumerate(dets):
+        # Get the landmarks/parts for the face in box d.
+        shape = predictor(img, d)
+        # Draw the face landmarks on the screen.
+        for p in shape.parts():
+            landmarks.append([p.x, p.y])
+        print(len(landmarks))
+        x1=landmarks[18][0];y1=landmarks[24][1]-landmarks[27][1]+landmarks[30][1]
+        x2=landmarks[25][0];y2=landmarks[25][1];y1=2*y2-y1###额头皱纹;
+        x3=landmarks[21][0];y3=landmarks[21][1]*2-landmarks[25][1]
+        x4=landmarks[22][0];y4=landmarks[25][1]###眉毛间
+        x5=landmarks[2][0];y5=landmarks[2][1]
+        x6=landmarks[39][0];y6=landmarks[41][1]*2-landmarks[39][1]###左眼眼下
+        x7=landmarks[42][0];y7=landmarks[46][1]*2-landmarks[44][1]
+        x8=landmarks[14][0];y8=landmarks[14][1]###右眼眼下
+        x9=landmarks[21][0];y9=landmarks[27][1]
+        x10=landmarks[22][0];y10=landmarks[30][1]###鼻子
+        x=[x1,x2,x3,x4,x5,x6,x7,x8,x9,x10]
+        y=[y1,y2,y3,y4,y5,y6,y7,y8,y9,y10]
+        mask = np.zeros(gray.shape[:2], dtype="uint8")
+        # mask[left:right,top:bottom]=255
+        mask[0:0, 0:0] = 255
+        image = cv2.bitwise_and(gray, gray, mask=mask)
+        theta=[0,math.pi/2,math.pi/4,math.pi/4*3]
+        for i in range(1):
+            g=gray
+            left=min(x[2*i],x[2*i+1])
+            right=max(x[2*i],x[2*i+1])
+            top=min(y[2*i],y[2*i+1])
+            bottom=max(y[2*i],y[2*i+1])
+            mask = np.zeros(g.shape[:2], dtype="uint8")
+            mask[top:bottom, left:right] = 255
+            g= cv2.bitwise_and(g, g, mask=mask)
+            cropped_image=gray[top:bottom, left:right]
+            result.append(face_wrinkle(cropped_image,theta=theta[i]))
+            image=cv2.add(image,g)
+        # cv2.imshow('result', image)
+        # cv2.waitKey(0)
+    return result
 
 if __name__ == '__main__':
-    path = r"aaa.jpg"
-    theta=0     ################ 0检测横向皱纹   math.pi/2检测竖向皱纹    math.pi/4 | math.pi/4*3 检测斜向皱纹
-    count=face_wrinkle(path,theta)
-    print(count)
+    predictor_path = "C:/Users/wry/Desktop/shape_predictor_68_face_landmarks.dat"
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(predictor_path)
+    image_path = "C:/Users/wry/Desktop/123.jpg"
+    ################ 0检测横向皱纹   math.pi/2检测竖向皱纹    math.pi/4 | math.pi/4*3 检测斜向皱纹
+    result=get_Wrinkle_vector(image_path)
+    # img=cv2.imread(image_path)
+    # result=face_wrinkle(img)
+    print(result)
+
 
